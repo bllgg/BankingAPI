e67f87f459e32a118fc708e080dadbf629b2e089
Shadow <gunathilakebllg@gmail.com>
2018-11-15 20:56:46 +0530

    Agent Update completed

--- objects/agent.php
+++ objects/agent.php
@@ -72,5 +72,44 @@ class Agent{
         return false;
         
     }
+
+    // update the agent
+    function update(){
+
+        $query = "UPDATE
+                bankingagents
+                SET NIC=:NIC, telephone=:telephone, name=:name, address=:address, agent_details=:agent_details, branch_number=:branch_number
+                WHERE agent_id=:agent_id";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->agent_id=htmlspecialchars(strip_tags($this->agent_id));
+        $this->NIC=htmlspecialchars(strip_tags($this->NIC));
+        $this->telephone=htmlspecialchars(strip_tags($this->telephone));
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->address=htmlspecialchars(strip_tags($this->address));
+        $this->agent_details=htmlspecialchars(strip_tags($this->agent_details));
+        $this->branch_number=htmlspecialchars(strip_tags($this->branch_number));
+    
+        // bind values
+        $stmt->bindParam(":agent_id", $this->agent_id);
+        $stmt->bindParam(":NIC", $this->NIC);
+        $stmt->bindParam(":telephone", $this->telephone);
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":address", $this->address);
+        $stmt->bindParam(":agent_details", $this->agent_details);
+        $stmt->bindParam(":branch_number", $this->branch_number);
+    
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+    
+        return false;
+        
+    }
+
 }
 ?>
