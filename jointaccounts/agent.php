814b6d995c401b37a2f214567c21b2630d20e9f4
Shadow <gunathilakebllg@gmail.com>
2018-11-15 18:26:02 +0530

    agent class created inside the agent folder

---
+++ jointaccounts/agent.php
@@ -0,0 +1,18 @@
+<?php
+class Agent{
+    
+    // database connection and the table name
+    private $conn;
+    private $table_name = "agent";
+
+    // object propoties
+    public $agent_id;
+    public $nic_number;
+    public $telephone_number;
+    public $name;
+    public $address;
+    public $agent_details;
+    public $branch_number;
+
+    
+}
