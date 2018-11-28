a7dda5e91a25be012391f181ffe640e9a4acf940
Shadow <gunathilakebllg@gmail.com>
2018-11-28 16:41:08 +0530

    Account holders updated to newvariable names.

--- accountholders/read.php
+++ accountholders/read.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 // include database and object files
 include_once '../config/database.php';
-include_once '../objects/accountholder.php';
+include_once '../objects/accountholders.php';
  
 // instantiate database and customer object
 $database = new Database();
